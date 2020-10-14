f94477503b58b16ed8374ad0004592fef233ec9f
Malik DeJean <mdejean@MacBook-Pro.local>
2020-10-14 12:16:15 -0700

    vercel modifications for deployment

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
 export default {
-    API_ENDPOINT: 'https://young-garden-84985.herokuapp.com/api',
-    TOKEN_KEY: 'techFighter-client-auth-token',
-  }
-  
+  API_ENDPOINT: 'https://young-garden-84985.herokuapp.com/api',
+  TOKEN_KEY: process.env.REACT_APP_API_KEY,
+}
